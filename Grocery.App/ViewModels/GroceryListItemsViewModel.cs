a0259658ea5df7922461be4317881469a10808ad
Jos Rigterink <rigterinkjos@gmail.com>
2025-09-18 19:51:30 +0200

    UC05 comments toegevoegd
    Comments toegevoegd aan de GetAvailableProducts en Addproduct functies om duidelijk aan te geven wat er gebeurt. bijvoorbeeld nieuw list aanmaken lijst bijwerken en vooraad updaten

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -36,25 +36,22 @@ namespace Grocery.App.ViewModels
         {
 
             AvailableProducts.Clear();
+            //GetAll haalt alle producten op
             var allproducts = _productService.GetAll();
 
             foreach (var product in allproducts)
             { 
+                //als er geen stock is slaat hij het over en gaat verder
                 if(product.Stock <= 0) continue;
 
+                //bool check of het product al op het boodschappenlijstje staat
                 bool alreadyInlist = MyGroceryListItems.Any(item => item.ProductId == product.Id);
 
                 if (alreadyInlist)
                 {
                     AvailableProducts.Add(product);
                 }
-
-            }
-
-            //Maak de lijst AvailableProducts leeg
-            //Haal de lijst met producten op
-            //Controleer of het product al op de boodschappenlijst staat, zo niet zet het in de AvailableProducts lijst
-            //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).            
+            }         
         }
 
         partial void OnGroceryListChanged(GroceryList value)
@@ -76,21 +73,18 @@ namespace Grocery.App.ViewModels
                 return;
             }
 
+            //nieuw GroceryListItem aanmaken
             var newItem = new GroceryListItem(0, GroceryList.Id, product.Id, 1);
 
+            //opgeslagen via service
             _groceryListItemsService.Add(newItem);
 
+            //vooraad updaten
             product.Stock--;
             _productService.Update(product);
 
+            //lijst bijwerken.
             OnGroceryListChanged(GroceryList);
-
-            //Controleer of het product bestaat en dat de Id > 0
-            //Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
-            //Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
-            //Werk de voorraad (Stock) van het product bij en zorg dat deze wordt vastgelegd (middels _productService)
-            //Werk de lijst AvailableProducts bij, want dit product is niet meer beschikbaar
-            //call OnGroceryListChanged(GroceryList);
         }
     }
 }
